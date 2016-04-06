48fc5604babf56935af356ab0c9d01eb87f547d4
Charles Lee <charleslee94@berkeley.edu>
2016-04-06 16:44:26 -0700

    added a check for dlc and added a return statement that provides feedback

--- reddit_bot.py
+++ reddit_bot.py
@@ -1,14 +1,11 @@
 import praw
 import codecs
 import re
-from datetime  import datetime
-from threading import Timer
+import os
+import random
+from datetime import datetime
 
 
-x = datetime.today()
-y = x.replace(day = x.day + 1, hour = 10, minute = 0, second = 0, microsecond = 0)
-delta_t = y - x
-secs = delta_t.seconds + 1
 
 def find_ds2_sale():
     user_agent = ("Dark Souls 2 sale finder")
@@ -17,9 +14,19 @@ def find_ds2_sale():
     output = codecs.open('output.txt', 'w', 'utf-8')
     for submission in subreddit.get_hot(limit=20):
         if re.findall('''Dark\s?Souls\s?(2|two)?''', submission.title, re.I):
+            if re.findall('''Scholar\s?of\s?the\s?first\s?sin''', submission.title, re.I):
+                output.write (" This is the DLC, THIS IS WHAT YOU WANT")
             output.write("Title: %s \n" % submission.title)
             output.write("Url: %s \n" % submission.url)
             output.write("-------------------------------------------------\n")
+    if os.stat("output.txt").st_size == 0:
+       output.write("No Dark souls 2 sales found in the hottest 20 submission on r/gamedeals on  %s \n" % datetime.today())
+       ds3_threshold = random.randrange(1,10)
+       if ds3_threshold >= 5:
+           output.write("Forget ds2, you should just get ds3 instead \n")
+       else:
+           output.write("Keep trying!\n")
+    output.write("This script will run again at 10 am tomorrow")
 
-t = Timer(secs, find_ds2_sale())
-t.start()
+
+find_ds2_sale()
